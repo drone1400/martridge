89df4f052200654d01f71ac9b5ea12f98b0bc608
drone1400 <>
2025-04-12 21:16:52 +0300

    SettingsGeneralView now switches layout based on height

---
+++ martridge/src/GenericTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Metadata;
+
+namespace Martridge {
+    public class GenericTemplateSelector : IDataTemplate {
+        
+        [Content]
+        public Dictionary<string, IDataTemplate> Templates { get; } = new Dictionary<string, IDataTemplate>();
+        
+        
+
+        public Control? Build(object? param) {
+            string key = param?.ToString()?.ToLowerInvariant() ?? string.Empty;
+            if (this.Templates.TryGetValue(key, out IDataTemplate? template)) {
+                return template.Build(param);
+            }
+            return null;
+        }
+        public bool Match(object? data) => true;
+    }
+}
---
+++ martridge/src/ViewModels/ValueConverters/DoubleCompareBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+namespace Martridge.ViewModels.ValueConverters {
+    public class DoubleCompareBoolConverter : IValueConverter {
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            if (value is double size && parameter is double threshold) {
+                return size > threshold;
+            }
+            // default to true?
+            return true;
+        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}

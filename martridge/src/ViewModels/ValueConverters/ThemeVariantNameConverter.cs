e3f012a71e2039067699a62a0530ee510bbbbb15
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-05-01 23:54:22 +0300

    Adjusted the way custom theme names are displayed

---
+++ martridge/src/ViewModels/ValueConverters/ThemeVariantNameConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+namespace Martridge.ViewModels.ValueConverters {
+    public class ThemeVariantNameConverter : IValueConverter {
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            if (value == null) return string.Empty;
+            return value.ToString()?.Replace('_', ' ');
+        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}

434bcfa616ffb6d6ac436930e9a122bfad12575d
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-05-08 19:26:31 +0300

    Hiding some DMOD Editor related settings when ShowDmodDevFeatures is false

---
+++ martridge/src/ViewModels/ValueConverters/BoolToGridSizeZeroStarConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+namespace Martridge.ViewModels.ValueConverters {
+    public class BoolToGridSizeZeroStarConverter : IValueConverter {
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            if (value is bool boolValue && boolValue) {
+                return new GridLength(1, GridUnitType.Star);
+            }
+            return new GridLength(0);
+        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}

cd8a11e0a4e8f3f5a362d55b1eff4676902dda6b
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-05-17 19:14:20 +0300

    Updated About page UI

--- martridge/src/ViewModels/About/AboutUsedPackageViewModel.cs
+++ martridge/src/ViewModels/About/AboutUsedPackageViewModel.cs
@@ -1,11 +1,33 @@
+using System.Collections.Generic;
 namespace Martridge.ViewModels.About {
-    public class AboutUsedPackageViewModel : ViewModelBase{
-        public string PackageName { get; }
-        public string PackageInfo { get; }
 
-        public AboutUsedPackageViewModel(string name, string info) {
-            this.PackageName = name;
-            this.PackageInfo = info;
+    public class UsedPackageLinkViewModel {
+        public string LinkType { get; }
+        public string Link { get; }
+        public UsedPackageLinkViewModel() {
+            this.LinkType = "Website";
+            this.Link = "https://127.0.0.1";
+        }
+        public UsedPackageLinkViewModel(string linkType, string link) {
+            this.LinkType = linkType;
+            this.Link = link;
+        }
+    }
+    public class AboutUsedPackageViewModel {
+        public string Name { get; }
+        public string Description { get; }
+        public List<UsedPackageLinkViewModel> Links { get; }
+
+        public AboutUsedPackageViewModel() {
+            this.Name = "placeholder";
+            this.Description = "...";
+            this.Links = new List<UsedPackageLinkViewModel>() { new UsedPackageLinkViewModel() };
+        }
+
+        public AboutUsedPackageViewModel(string name, string description, List<UsedPackageLinkViewModel> links) {
+            this.Name = name;
+            this.Description = description;
+            this.Links = links;
         }
         
     }
--- martridge/src/ViewModels/About/AboutViewModel.cs
+++ martridge/src/ViewModels/About/AboutViewModel.cs
@@ -5,8 +5,8 @@ using Martridge.ViewModels.DinkyGraphics;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reflection;
-using Martridge.Trace;
 
 namespace Martridge.ViewModels.About {
     public class AboutViewModel : ViewModelAppPage
@@ -52,6 +52,26 @@ namespace Martridge.ViewModels.About {
         }
         
         public string Version { get; }
+        
+        public void CmdOpenHyperlink(object? parameter = null) {
+            if (parameter is not string str ||
+                string.IsNullOrWhiteSpace(str) ||
+                str.ToLowerInvariant().StartsWith("https://") == false)
+                return;
+            
+            ProcessStartInfo pinfo = new ProcessStartInfo(str) {
+                UseShellExecute = true,
+                Verb = "open",
+            };
+            Process.Start(pinfo);
+        }
+        public bool CanCmdOpenHyperlink(object? parameter = null) {
+            if (parameter is not string str ||
+                string.IsNullOrWhiteSpace(str) ||
+                str.ToLowerInvariant().StartsWith("https://") == false)
+                return false;
+            return true;
+        }
 
         public AboutViewModel() {
             Version? version = Assembly.GetExecutingAssembly().GetName().Version;
@@ -61,44 +81,95 @@ namespace Martridge.ViewModels.About {
 
         private void InitializeLocalizedPackageInfo() {
             this._lastInitializedLanguage = this._cfg?.LocalizationName;
+
+            string linkWebsite = Localizer.Instance["AboutWindow/Package/LinkType/Website"];
+            string linkSource = Localizer.Instance["AboutWindow/Package/LinkType/Source"];
+            string linkOriginalSource = Localizer.Instance["AboutWindow/Package/LinkType/OriginalSource"];
+            string linkModifiedSource = Localizer.Instance["AboutWindow/Package/LinkType/ModifiedSource"];
+            
             
             this.UsedPackages = new List<AboutUsedPackageViewModel>() {
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/DinkNetwork/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/DinkNetwork/Description"]),
+                    Localizer.Instance["AboutWindow/Package/DinkNetwork/Name"],
+                    Localizer.Instance["AboutWindow/Package/DinkNetwork/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkWebsite, "https://www.dinknetwork.com/"),
+                    }),
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/DinkGraphics/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/DinkGraphics/Description"]),
+                    Localizer.Instance["AboutWindow/Package/DinkGraphics/Name"],
+                    Localizer.Instance["AboutWindow/Package/DinkGraphics/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkWebsite, "https://www.dinknetwork.com/file/dink_smallwood_bmp_graphics"),
+                    }),
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/AvaloniaUI/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/AvaloniaUI/Description"]),
+                    Localizer.Instance["AboutWindow/Package/Icons8/Name"],
+                    Localizer.Instance["AboutWindow/Package/Icons8/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkWebsite, "https://icons8.com/"),
+                    }),
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/Citrus.Avalonia/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/Citrus.Avalonia/Description"]),
+                    Localizer.Instance["AboutWindow/Package/AvaloniaUI/Name"],
+                    Localizer.Instance["AboutWindow/Package/AvaloniaUI/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkWebsite, "https://avaloniaui.net/"),
+                        new UsedPackageLinkViewModel(linkSource, "https://github.com/AvaloniaUI/Avalonia"),
+                    }),
+                new AboutUsedPackageViewModel(
+                    Localizer.Instance["AboutWindow/Package/Citrus.Avalonia/Name"],
+                    Localizer.Instance["AboutWindow/Package/Citrus.Avalonia/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkOriginalSource, "https://github.com/AvaloniaUI/Citrus.Avalonia"),
+                        new UsedPackageLinkViewModel(linkModifiedSource, "https://github.com/drone1400/Citrus.Avalonia"),
+                    }),
 #if PLATF_WINDOWS && ENABLE_FEATURE_DINK_INSTALLER
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/SevenZipExtractor/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/SevenZipExtractor/Description"]),
+                    Localizer.Instance["AboutWindow/Package/SevenZipExtractor/Name"],
+                    Localizer.Instance["AboutWindow/Package/SevenZipExtractor/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkSource, "https://github.com/adoconnection/SevenZipExtractor"),
+                    }),
 #endif
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/SharpCompress/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/SharpCompress/Description"]),
+                    Localizer.Instance["AboutWindow/Package/SharpCompress/Name"],
+                    Localizer.Instance["AboutWindow/Package/SharpCompress/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkOriginalSource, "https://github.com/adamhathcock/sharpcompress"),
+                        new UsedPackageLinkViewModel(linkModifiedSource, "https://github.com/drone1400/sharpcompress/tree/drone-modifications-2024"),
+                    }),
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/Bzip2/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/Bzip2/Description"]),
+                    Localizer.Instance["AboutWindow/Package/Bzip2/Name"],
+                    Localizer.Instance["AboutWindow/Package/Bzip2/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkOriginalSource,"https://github.com/jaime-olivares/bzip2"),
+                        new UsedPackageLinkViewModel(linkModifiedSource, "https://github.com/drone1400/bzip2"),
+                    }),
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/NewtonsoftJson/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/NewtonsoftJson/Description"]),
+                    Localizer.Instance["AboutWindow/Package/NewtonsoftJson/Name"],
+                    Localizer.Instance["AboutWindow/Package/NewtonsoftJson/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkWebsite, "https://www.newtonsoft.com/json"),
+                        new UsedPackageLinkViewModel(linkSource, "https://github.com/JamesNK/Newtonsoft.Json"),
+                    }),
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/GetText/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/GetText/Description"]),
+                    Localizer.Instance["AboutWindow/Package/GetText/Name"],
+                    Localizer.Instance["AboutWindow/Package/GetText/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkSource, "https://github.com/perpetualKid/GetText.NET"),
+                    }),
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/Ignore/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/Ignore/Description"]),
+                    Localizer.Instance["AboutWindow/Package/Ignore/Name"],
+                    Localizer.Instance["AboutWindow/Package/Ignore/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkSource, "https://github.com/goelhardik/ignore"),
+                    }),
 #if ENABLE_FEATURE_ONLINE
                 new AboutUsedPackageViewModel(
-                    name: Localizer.Instance[@"AboutWindow/Package/HtmlAgilityPack/Name"],
-                    info: Localizer.Instance[@"AboutWindow/Package/HtmlAgilityPack/Description"]),
+                    Localizer.Instance["AboutWindow/Package/HtmlAgilityPack/Name"],
+                    Localizer.Instance["AboutWindow/Package/HtmlAgilityPack/Description"],
+                    new List<UsedPackageLinkViewModel>() {
+                        new UsedPackageLinkViewModel(linkWebsite, "https://html-agility-pack.net/"),
+                        new UsedPackageLinkViewModel(linkSource, "https://github.com/zzzprojects/html-agility-pack"),
+                    }),
 #endif
             };
         }

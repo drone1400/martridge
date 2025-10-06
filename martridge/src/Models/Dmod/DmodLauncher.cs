d4f428374a995a8eec2e5221e7f57b6b2b6b55d4
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-10-06 23:46:04 +0300

    Changes to DmodLauncher - supports launching DMODs using a Dink executable that is itself launched through Steam using the Steam URI format "steam://run/<id>//<args>/" - supports launching DMODs using flatpak or other such esoteric methods - some other minor adjustments...

--- martridge/src/Models/Dmod/DmodLauncher.cs
+++ martridge/src/Models/Dmod/DmodLauncher.cs
@@ -15,22 +15,30 @@ namespace Martridge.Models.Dmod
         public static void LaunchDmod(string exePath, string dmodPath, ConfigLaunch launch, bool quitAfterLaunching, string? localization = null)
         {
             try {
+                exePath = exePath.Trim();
+                dmodPath = dmodPath.Trim();
+                
                 bool isProbablyFreeDink = false;
                 bool isProbablyYeOldeDink = false;
                 
                 FileInfo finfo = new FileInfo(exePath);
-                DirectoryInfo dinfo = new DirectoryInfo(dmodPath);
-                string launcherExeNameLower = Path.GetFileNameWithoutExtension(finfo.Name.ToLowerInvariant());
+                string launcherExeNameLower = Path.GetFileNameWithoutExtension(finfo.Exists ? finfo.Name.ToLowerInvariant() : string.Empty);
+                string exePathLower = exePath.ToLowerInvariant();
 
-                if (launcherExeNameLower.StartsWith("freedink")) {
+                if (launcherExeNameLower.StartsWith("freedink")         // executable file that's freedink
+                ) {
                     isProbablyFreeDink = true;
                 }
-                if (launcherExeNameLower.StartsWith("yedink") ||
-                    launcherExeNameLower.StartsWith("yeoldedink")) {
+                if (launcherExeNameLower.StartsWith("yedink") ||        // executable file that's yeoldedink
+                    launcherExeNameLower.StartsWith("yeoldedink") ||    // executable file that's yeoldedink
+                    exePathLower.Contains("net.ultraprison.yeoldedink") // for yeoldedink flatpaks
+                ) {
                     isProbablyYeOldeDink = true;
                 }
                 
                 string arguments = "";
+                
+                // appending the classic dink arguments
                 if (launch.TrueColor) {
                     arguments += " -truecolor";
                 }
@@ -56,26 +64,30 @@ namespace Martridge.Models.Dmod
                 if (launch.Skip && isProbablyFreeDink == false && isProbablyYeOldeDink == false) {
                     arguments += " -skip";
                 }
-                
-                if (dinfo.Name.ToLowerInvariant() != "dink") {
-                    string finalPath = dmodPath;
-                    
-                    if (launch.UsePathRelativeToGame && finfo.DirectoryName != null) {
-                        finalPath = Path.GetRelativePath(finfo.DirectoryName, dmodPath);
-                    }
-                    
-                    finalPath = Path.TrimEndingDirectorySeparator(finalPath);
 
-                    // NOTE: If path contains whitespace, force quotation marks on since otherwise you can't launch the DMOD
-                    if (launch.UsePathQuotationMarks ||
-                        finalPath.Any(c => Char.IsWhiteSpace(c))) {
-                        finalPath = $"\"{finalPath}\"";
+                // appending the -game path argument if using one
+                if (string.IsNullOrWhiteSpace(dmodPath) == false) {
+                    DirectoryInfo dinfo = new DirectoryInfo(dmodPath);
+                    if (dinfo.Name.ToLowerInvariant() != "dink") {
+                        string finalPath = dmodPath;
+
+                        if (launch.UsePathRelativeToGame && finfo.Exists && finfo.DirectoryName != null) {
+                            finalPath = Path.GetRelativePath(finfo.DirectoryName, dmodPath);
+                        }
+
+                        finalPath = Path.TrimEndingDirectorySeparator(finalPath);
+
+                        // NOTE: If path contains whitespace, force quotation marks on since otherwise you can't launch the DMOD
+                        if (launch.UsePathQuotationMarks || finalPath.Any(Char.IsWhiteSpace)) {
+                            finalPath = $"\"{finalPath}\"";
+                        }
+
+                        arguments += " -game ";
+                        arguments += finalPath;
                     }
-                    
-                    arguments += " -game ";
-                    arguments += finalPath;
                 }
 
+                // appending the --refdir path argument if needed
                 if (launch.UseRefDir && string.IsNullOrWhiteSpace(launch.RefDirPath) == false) {
                     string refDirPath = Path.TrimEndingDirectorySeparator(launch.RefDirPath);
                     
@@ -88,28 +100,33 @@ namespace Martridge.Models.Dmod
                     }
                 }
 
+                // appending custom user arguments
                 if (!string.IsNullOrWhiteSpace(launch.CustomUserArguments)) {
                     arguments += " ";
                     arguments += launch.CustomUserArguments;
                 }
 
-                MyTrace.Global.WriteMessage(new List<string>() {
-                    "<Attempting to start dmod>",
-                    $"    Dink = \"{exePath}\"",
-                    $"    Args = {arguments}",
-                    });
-
-                ProcessStartInfo pinfo = new ProcessStartInfo()
-                {
-                    FileName = exePath,
-                    Arguments = arguments,
-                    WorkingDirectory = finfo.Directory?.FullName,
-                };
+                // arguments are done!
+                arguments = arguments.Trim();
                 
-                // localization support for freedink
-                // localization = "es_ES";
-                if (isProbablyFreeDink)
-                {
+                ProcessStartInfo pinfo = new ProcessStartInfo();
+                string finalFileName = "";
+                string finalArguments = "";
+                
+                if (finfo.Exists) {
+                    // ---------------------------------------------------
+                    // starting from a normal executable
+                    // ---------------------------------------------------
+
+                    finalFileName = exePath;
+                    finalArguments = arguments;
+                    
+                    pinfo.FileName = finalFileName;
+                    pinfo.Arguments = finalArguments;
+                    pinfo.WorkingDirectory = finfo.Directory?.FullName;
+                    
+                    // localization support for freedink/yeoldedink
+                    
                     // try to add localization parameters
                     if (localization != null) {
                         pinfo.Environment.RemoveIfContained("LANGUAGE");
@@ -118,14 +135,83 @@ namespace Martridge.Models.Dmod
                         pinfo.Environment.Add("LC_ALL", localization);
                         pinfo.Environment.Add("LANGUAGE", localization);
                     }
-
-                    // this fixes a sound issue regarding playback of WAV files with FreeDink 109.6 under Windows 10 and 11
-                    if (finfo.Extension == ".exe") {
+                    
+                    
+                    if (isProbablyFreeDink && finfo.Extension == ".exe")
+                    {
+                        // this fixes a sound issue regarding playback of WAV files with FreeDink 109.6 under Windows 10 and 11
                         pinfo.Environment.RemoveIfContained("SDL_AUDIODRIVER");
                         pinfo.Environment.Add("SDL_AUDIODRIVER", "winmm");
                     }
-                }
                 
+                    MyTrace.Global.WriteMessage(new List<string>() {
+                        Localization.Localizer.Instance["DmodLauncher/LaunchFile"],
+                        $"    FileName = \"{finalFileName}\"",
+                        $"    Arguments = {finalArguments}",
+                    });
+                }
+                else if (exePathLower.StartsWith("steam://")) {
+                    // ---------------------------------------------------
+                    // starting from a steam uri
+                    // ---------------------------------------------------
+                    
+                    // have to respect "steam://run/<id>//<args>/" URI format to launch something through steam with arguments....
+                    // slashes have to be escaped with %2F
+                    string argumentsEscaped = arguments.Trim().Replace("/", "%2F");
+                    finalFileName = exePath + "//" + argumentsEscaped + "/";
+                    finalArguments = "";
+                    pinfo.FileName = finalFileName;
+                    pinfo.Arguments = finalArguments;
+                    pinfo.UseShellExecute = true;
+                    pinfo.Verb = "open";
+                    
+                    MyTrace.Global.WriteMessage(new List<string>() {
+                        Localization.Localizer.Instance["DmodLauncher/LaunchUsingSteam"],
+                        $"    FileName = \"{finalFileName}\"",
+                        $"    Arguments = {finalArguments}",
+                    });
+                } else {
+                    // ---------------------------------------------------
+                    // this is not a file or a steam uri, attempt launching it using shell execute with arguments?...
+                    // ---------------------------------------------------
+
+                    // NOTE: using only the first word or phrase as the "FileName", the rest are considered arguments...
+                    int stopIdx = 0;
+                    if (exePath.StartsWith('\"') || exePath.StartsWith('\'')) {
+                        // look for ending " or '
+                        // note: currently not allowing escaping the " or '...
+                        char stopChar = exePath[0];
+                        stopIdx++;
+                        while (stopIdx < exePath.Length) {
+                            if (exePath[stopIdx] == stopChar) break;
+                            stopIdx++;
+                        }
+                    } else {
+                        // look for whitespace
+                        stopIdx++;
+                        while (stopIdx < exePath.Length) {
+                            if (exePath[stopIdx] == ' ' || exePath[stopIdx] == '\t') break;
+                            stopIdx++;
+                        }
+                    }
+                    
+                    finalFileName = exePath.Substring(0,stopIdx).Trim();
+                    string arguments1 = exePath.Substring(stopIdx).Trim();
+                    finalArguments = string.IsNullOrWhiteSpace(arguments1)
+                        ? arguments : arguments1 + " " + arguments;
+                    
+                    pinfo.FileName = finalFileName;
+                    pinfo.Arguments = finalArguments;
+                    pinfo.UseShellExecute = true;
+                    pinfo.Verb = "open";
+                    
+                    MyTrace.Global.WriteMessage(new List<string>() {
+                        Localization.Localizer.Instance["DmodLauncher/LaunchGeneric"],
+                        $"    FileName = \"{finalFileName}\"",
+                        $"    Arguments = {finalArguments}",
+                    });
+                }
+
                 Process? proc = Process.Start(pinfo);
 
                 if (quitAfterLaunching) {
@@ -134,12 +220,11 @@ namespace Martridge.Models.Dmod
                 
                 proc?.WaitForExit();
                 MyTrace.Global.WriteMessage(new List<string>() {
-                    "<Dmod process ended>",
-                    $"    Dink      = \"{exePath}\"",
-                    $"    Args      = {arguments}",
+                    Localization.Localizer.Instance["DmodLauncher/LaunchEnd"],
+                    $"    FileName = \"{finalFileName}\"",
+                    $"    Arguments = {finalArguments}",
                     $"    Exit Code = {proc?.ExitCode}",
                 });
-
             } catch (Exception ex) {
                 MyTrace.Global.WriteException(ex);
             }

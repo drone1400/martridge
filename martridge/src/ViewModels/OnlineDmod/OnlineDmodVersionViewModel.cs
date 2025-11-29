0868f20ed962c49e1b05ed8ce6e928f603978628
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-11-29 23:02:28 +0200

    Internal changes to OnlineDmodVersionViewModel Properties

--- martridge/src/ViewModels/OnlineDmod/OnlineDmodVersionViewModel.cs
+++ martridge/src/ViewModels/OnlineDmod/OnlineDmodVersionViewModel.cs
@@ -1,11 +1,12 @@
+using System;
 using Martridge.Models.OnlineDmods;
 namespace Martridge.ViewModels.OnlineDmod {
     public class OnlineDmodVersionViewModel : ViewModelBase {
         private OnlineDmodVersion? _dmodVersion = null;
         
         public string Name { get => this._dmodVersion?.Name ?? string.Empty; }
-        public string Released { get => this._dmodVersion?.Released.ToString("d") ?? string.Empty; }
-        public string Downloads { get => this._dmodVersion?.Downloads.ToString() ?? string.Empty; }
+        public DateTime Released { get => this._dmodVersion?.Released ?? DateTime.MinValue; }
+        public int Downloads { get => this._dmodVersion?.Downloads ?? 0; }
         public string FileSizeString { get => this._dmodVersion?.FileSizeString ?? string.Empty; }
         public string ReleaseNotes { get => this._dmodVersion?.ReleaseNotes ?? string.Empty; }
         public string RelativeDownloadUrl {get => this._dmodVersion?.RelativeDownloadUrl ?? string.Empty; }

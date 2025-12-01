drone1400/martridge

commit 6f17aea9e1ff7df00b72c46821bf0c0fbc2a410f

Fixed issue with settings not being updated immediately after swapping to the DMOD browser leading to DMODs not being detected first time when adding a game engine exe

martridge/src/ViewModels/MainWindowViewModel.cs
@@ -368,6 +368,18 @@ namespace Martridge.ViewModels {
 
             ViewModelAppPage? newViewModel = this._dmodBrowserViewModel;
             
+            // make sure settings get applied if swapping here from settings VM
+            if (this._currentViewModel is SettingsGeneralViewModel settings) {
+                // apply settings...
+                settings.CmdSettingsOk();
+                // dispose old view model
+                settings.Dispose();
+            } else
+            {
+                // dispose current view model if needed
+                this._currentViewModel?.Dispose();
+            }
+            
             // check we have a valid game or editor exe
             if (this._config != null &&
                 this._config.General.GameExePaths.Count == 0 &&

commit af2f0a00f49eb62549705dc5409c0542a93646c6

Fixed nasty bug introduced in commit 6f17aea9 due to oversight

martridge/src/ViewModels/MainWindowViewModel.cs
@@ -372,12 +372,6 @@ namespace Martridge.ViewModels {
             if (this._currentViewModel is SettingsGeneralViewModel settings) {
                 // apply settings...
                 settings.CmdSettingsOk();
-                // dispose old view model
-                settings.Dispose();
-            } else
-            {
-                // dispose current view model if needed
-                this._currentViewModel?.Dispose();
             }
             
             // check we have a valid game or editor exe
64af0bd3850d9c8447b354b351d3cf2a4f1f7a1e
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-12-07 16:42:57 +0200

    Fixed framework version issue for sharpcompress in publish scripts

--- publish-linux-x64.ps1
+++ publish-linux-x64.ps1
@@ -4,6 +4,6 @@
 #publish linux-x64
 dotnet clean ./citrus/src/Citrus.Avalonia/Citrus.Avalonia.csproj --configuration Release -p:Platform="Any CPU" --framework netstandard2.0
 dotnet clean ./citrus/src/Citrus.Avalonia.DataGrid/Citrus.Avalonia.DataGrid.csproj --configuration Release -p:Platform="Any CPU" --framework netstandard2.0
-dotnet clean ./sharpcompress/src/SharpCompress/SharpCompress.csproj --configuration Release -p:Platform="Any CPU" --framework netstandard2.0
+dotnet clean ./sharpcompress/src/SharpCompress/SharpCompress.csproj --configuration Release -p:Platform="Any CPU" --framework net10.0
 dotnet clean ./martridge/src/martridge.csproj --configuration Release -p:Platform="Any CPU" --framework net10.0
 dotnet publish ./martridge/src/martridge.csproj --output ./publish/net10.0_linux-x64/martridge/ --configuration Release -p:Platform="Any CPU" --framework net10.0 --runtime linux-x64 --self-contained true -p:PublishSingleFile=true
--- publish-win-x86.ps1
+++ publish-win-x86.ps1
@@ -4,6 +4,6 @@
 #publish win-x86
 dotnet clean ./citrus/src/Citrus.Avalonia/Citrus.Avalonia.csproj --configuration Release -p:Platform="x86" --framework netstandard2.0
 dotnet clean ./citrus/src/Citrus.Avalonia.DataGrid/Citrus.Avalonia.DataGrid.csproj --configuration Release -p:Platform="x86" --framework netstandard2.0
-dotnet clean ./sharpcompress/src/SharpCompress/SharpCompress.csproj --configuration Release -p:Platform="x86" --framework netstandard2.0
+dotnet clean ./sharpcompress/src/SharpCompress/SharpCompress.csproj --configuration Release -p:Platform="x86" --framework net10.0
 dotnet clean ./martridge/src/martridge.csproj --configuration Release -p:Platform="x86" --framework net10.0
 dotnet publish ./martridge/src/martridge.csproj --output ./publish/net10.0_win-x86/martridge/ --configuration Release -p:Platform="x86" --framework net10.0 --runtime win-x86 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true

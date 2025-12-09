31c43247ee3b66e20d47b68cdbacf0ac0f6c1c5e
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-12-09 21:53:56 +0200

    DinkTempFileHelper changes - fixed issue with temporary directories not being deleted - fixed temporary file/directory deletion order - changed log entries to use two lines

--- martridge/src/Models/DinkTempFileHelper.cs
+++ martridge/src/Models/DinkTempFileHelper.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
-using DynamicData;
 using Martridge.Models.Localization;
 using Martridge.Trace;
 namespace Martridge.Models {
     public class DinkTempFileHelper : IDisposable
     {
 
-        public delegate void LogCallback(string message);
+        public delegate void LogCallback(string line1, string line2);
         
-        
-        public IReadOnlyList<FileInfo> TempFileList;
-        public IReadOnlyList<DirectoryInfo> TempDirectories;
-        
-        private readonly List<FileInfo> _tempFiles = new List<FileInfo>();
-        private readonly List<DirectoryInfo> _tempDirectories = new List<DirectoryInfo>();
+        private readonly Stack<FileInfo> _tempFiles = new Stack<FileInfo>();
+        private readonly Stack<DirectoryInfo> _tempDirectories = new Stack<DirectoryInfo>();
         private DirectoryInfo _tempBaseFolder;
         
         private bool _disposed = false;
@@ -24,8 +18,6 @@ namespace Martridge.Models {
         private LogCallback? _logCallback = null;
 
         public DinkTempFileHelper() {
-            this.TempFileList = new ReadOnlyCollection<FileInfo>(this._tempFiles);
-            this.TempDirectories = new ReadOnlyCollection<DirectoryInfo>(this._tempDirectories);
 
             this._tempBaseFolder = new DirectoryInfo(Path.Combine(Path.GetTempPath(), "dink_temp"));
             
@@ -33,7 +25,7 @@ namespace Martridge.Models {
                 this._tempBaseFolder.Create();
             }
             
-            this._tempDirectories.Add(this._tempBaseFolder);
+            this._tempDirectories.Push(this._tempBaseFolder);
         }
 
         public void SetLogCallback(LogCallback callback)
@@ -45,14 +37,22 @@ namespace Martridge.Models {
             this._logCallback = null;
         }
 
+        private void LogInvoke(string message, string filePath) {
+            try {
+                this._logCallback?.Invoke(message, $"    \"{filePath}\"");
+            } catch (Exception) {
+                // ignored
+            }
+        }
+
         public FileInfo? TryCreateTempFile() {
             try {
                 string filename = Path.GetRandomFileName();
                 string fullPath = Path.Combine(this._tempBaseFolder.FullName, filename);
-                this._logCallback?.Invoke(Localizer.Instance["DinkTempFileHelper/Log/CreatingTemporaryFile"] + fullPath);
+                this.LogInvoke(Localizer.Instance["DinkTempFileHelper/Log/CreatingTemporaryFile"], fullPath);
                 FileInfo temp = new FileInfo(fullPath);
                 using FileStream tempFs = temp.Create();
-                this._tempFiles.Add(temp);
+                this._tempFiles.Push(temp);
                 return temp;
             } catch (Exception) {
                 return null;
@@ -65,10 +65,10 @@ namespace Martridge.Models {
             {
                 string dirName = Path.GetRandomFileName();
                 string fullPath = Path.Combine(this._tempBaseFolder.FullName, dirName);
-                this._logCallback?.Invoke(Localizer.Instance["DinkTempFileHelper/Log/CreatingTemporaryDirectory"] + fullPath);
+                this.LogInvoke(Localizer.Instance["DinkTempFileHelper/Log/CreatingTemporaryDirectory"], fullPath);
                 DirectoryInfo temp = new DirectoryInfo(fullPath);
                 temp.Create();
-                this._tempDirectories.Add(temp);
+                this._tempDirectories.Push(temp);
                 return temp;
             } catch (Exception)
             {
@@ -78,7 +78,7 @@ namespace Martridge.Models {
 
         public void RegisterTempFile(FileInfo tempFile)
         {
-            this._tempFiles.Add(tempFile);
+            this._tempFiles.Push(tempFile);
         }
 
         public void Dispose() {
@@ -100,18 +100,25 @@ namespace Martridge.Models {
             
 
             try {
-                foreach (var x in this._tempFiles) {
+                while (this._tempFiles.Count > 0) {
                     try {
-                        this._logCallback?.Invoke(deleteFileMsg + x.FullName);
+                        FileInfo x = this._tempFiles.Pop();
+                        try {
+                            this.LogInvoke(deleteFileMsg, x.FullName);
+                        } catch (Exception) {
+                            // ignored
+                        }
                         x.Delete();
                     } catch (Exception ex) {
                         MyTrace.Global.WriteException(ex);
                     }
                 }
-                foreach (var x in this._tempDirectories) {
+
+                while (this._tempDirectories.Count > 0) {
                     try {
-                        this._logCallback?.Invoke(deleteDirectoryMsg + x.FullName);
-                        x.Delete();
+                        DirectoryInfo x = this._tempDirectories.Pop();
+                        this.LogInvoke(deleteDirectoryMsg, x.FullName);
+                        x.Delete(true);
                     } catch (Exception ex) {
                         MyTrace.Global.WriteException(ex);
                     }

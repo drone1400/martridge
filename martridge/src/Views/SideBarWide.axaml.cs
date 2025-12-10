7cd4760af6bb92ea119bc969daa863d0323a21ea
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-12-10 23:21:20 +0200

    Moved left sidebar AXAML code to separate file

---
+++ martridge/src/Views/SideBarWide.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Martridge.Views {
+    public partial class SideBarWide : UserControl {
+        public SideBarWide() {
+            InitializeComponent();
+        }
+    }
+}
+

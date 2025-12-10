30a5b7a7406ee442e416e8e531fdeb7c6b105f9f
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-12-10 23:53:22 +0200

    Left sidebar now switches to new narrow-mode when window is too narrow

---
+++ martridge/src/Views/SideBarNarrow.axaml.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Controls;
+
+namespace Martridge.Views {
+    public partial class SideBarNarrow : UserControl {
+        public SideBarNarrow() {
+            InitializeComponent();
+        }
+    }
+}
+

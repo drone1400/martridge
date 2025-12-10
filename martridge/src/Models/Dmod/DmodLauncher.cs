0dac8a59f44ed44f4b4a881c6df0cacde33e4d07
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-12-10 16:20:17 +0200

    When launching a DMOD, the process being launched will use the DMOD's directory as the working directory

--- martridge/src/Models/Dmod/DmodLauncher.cs
+++ martridge/src/Models/Dmod/DmodLauncher.cs
@@ -73,7 +73,10 @@ namespace Martridge.Models.Dmod
                     pinfo.Environment.Add("LC_ALL", localization);
                     pinfo.Environment.Add("LANGUAGE", localization);
                 }
-
+                
+                // use the DMOD directory as the working directory when launching 
+                pinfo.WorkingDirectory = dmodPath;
+                
                 Process? proc = Process.Start(pinfo);
 
                 // check if need to quit martridge after launching

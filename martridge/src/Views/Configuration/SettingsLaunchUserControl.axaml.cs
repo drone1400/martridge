be603fd2123a8d73c74f18ecc20169efb25ea39a
drone1400 <93078773+drone1400@users.noreply.github.com>
2025-12-11 01:24:43 +0200

    Settings UI changes - moved launch settings to separate tab

---
+++ martridge/src/Views/Configuration/SettingsLaunchUserControl.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Martridge.Views.Configuration {
+    public partial class SettingsLaunchUserControl : UserControl {
+        public SettingsLaunchUserControl() {
+            InitializeComponent();
+        }
+    }
+}
+
